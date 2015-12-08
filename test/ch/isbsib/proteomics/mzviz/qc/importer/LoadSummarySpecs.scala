76b9accee78d330d0f6f067610626980060618b2
qinfang jolliet <shinhou.ko@gmail.com>
2015-12-08 11:44:16 +0100

    update QcSummary test

--- test/ch/isbsib/proteomics/mzviz/qc/importer/LoadSummarySpecs.scala
+++ test/ch/isbsib/proteomics/mzviz/qc/importer/LoadSummarySpecs.scala
@@ -21,8 +21,8 @@ class LoadSummarySpecs extends Specification with ScalaFutures{
     val summaryEntry = qcSummary.getSummaryEntry
     val n = summaryEntry.length
 
-    "numberOfEntries should be 2" in {
-      n must equalTo(2)
+    "numberOfEntries should be 5" in {
+      n must equalTo(5)
     }
     "rawfile Date should be yymmdd " in {
       summaryEntry(0).rawfileInfomation.Date.value must equalTo("150508")
@@ -42,8 +42,15 @@ class LoadSummarySpecs extends Specification with ScalaFutures{
     "rawfile peptideSeq should be 57 " in {
       summaryEntry(0).PeptideSeq must equalTo(57)
     }
-
-
+    "rawfile peptideSeq should be 6.56 " in {
+      summaryEntry(0).MMSIdentifyPtg must equalTo(6.56)
+    }
+    "rawfile Peaks Repeatedly Sequenced should be 7.08 " in {
+      summaryEntry(0).PkRepSeqPtg must equalTo(7.08)
+    }
+    "rawfile Cmt should be '' " in {
+      summaryEntry(0).Cmt must equalTo("")
+    }
 
     "get information from rawfile" should {
       val raw_file = "Hela_300ng_Qex_Dionex_00_150507_00"
--- test/ch/isbsib/proteomics/mzviz/qc/services/SummaryMongoDBSerivcesSpecs.scala
+++ test/ch/isbsib/proteomics/mzviz/qc/services/SummaryMongoDBSerivcesSpecs.scala
@@ -1,12 +1,18 @@
 package ch.isbsib.proteomics.mzviz.qc.services
 
 import ch.isbsib.proteomics.mzviz.commons.TempMongoDBForSpecs
+import ch.isbsib.proteomics.mzviz.qc._
 import ch.isbsib.proteomics.mzviz.qc.importer.LoadSummary
+import ch.isbsib.proteomics.mzviz.qc.models.RawfileInfomation
+
 import org.specs2.mutable.Specification
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Seconds, Span}
 import org.specs2.mutable.Specification
 import ch.isbsib.proteomics.mzviz.qc.services.JsonQCFormats._
+import play.api.libs.json.Json
+
+import scala.util.parsing.json.JSON
 
 /**
  * Created by qjolliet on 05/08/15.
@@ -27,16 +33,23 @@ class SummaryMongoDBSerivcesSpecs extends Specification with ScalaFutures {
     "insert  2" in new TempMongoDBService {
       val entries = LoadSummary("./test/resources/qc/summary.txt").getSummaryEntry
       val n: Int = service.insert(entries).futureValue
-      n must equalTo(2)
+      n must equalTo(5)
 
       val qcSummaryEntry = service.findAllByDate("150508").futureValue
       Thread.sleep(200)
       qcSummaryEntry.size must equalTo(2)
 
+      val idx = RawfileInfomation(ProteinName("Hela"),ProteinQuantity("300ng"),MachineName("Qex"),ColumnType("Dionex_00"),QcDate("150508"),QcIndex("00"))
+      //val idx=Json.toJson("'proteinName':'Hela','pQuantity':'300ng','machineName':'Qex','columnType':'Dionex_00','Date':'150507','Index':'00'")
+      val b: Boolean = service.updateCmtByRawfileInfo(idx,"hello").futureValue
+      Thread.sleep(200)
+      b must equalTo(true)
+
     }
+
   }
 
-
+/*
   "Insert 13,delete 3 " should {
     "find 4" in new TempMongoDBService {
       val ins1 = service.insert(LoadSummary("./test/resources/qc/summary1.txt").getSummaryEntry).futureValue
@@ -50,6 +63,17 @@ class SummaryMongoDBSerivcesSpecs extends Specification with ScalaFutures {
       val qcSummaryEntry = service.findAllByDate("150520").futureValue
       Thread.sleep(200)
       qcSummaryEntry.size must equalTo(4)
+
+      //val idx = RawfileInfomation(ProteinName("Hela"),ProteinQuantity("300ng"),MachineName("Qex"),ColumnType("Dionex_00"),QcDate("150507"),QcIndex("00"))
+      val idx=Json.toJson("'proteinName':'Hela','pQuantity':'300ng','machineName':'Qex','columnType':'Dionex_00','Date':'150507','Index':'00'")
+      val b: Boolean = service.updateCmtByRawfileInfo(idx,"hello").futureValue
+      Thread.sleep(200)
+      b must equalTo(true)
+
+
+      //val upd=service.updateByRawfileInfo(idx,"hello").futureValue
+
+
     }
 
   }
@@ -77,6 +101,6 @@ class SummaryMongoDBSerivcesSpecs extends Specification with ScalaFutures {
       qcSummaryEntry.size must equalTo(13)
     }
 
-  }
+  }*/
 
 }
