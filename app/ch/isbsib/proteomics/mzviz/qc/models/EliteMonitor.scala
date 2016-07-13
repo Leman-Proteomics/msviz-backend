241545f0fa5bbfe805b6c7cf55cab675f834ef61
qinfang jolliet <shinhou.ko@gmail.com>
2016-07-13 13:55:08 +0200

    create EliteMonitorInfo model

---
+++ app/ch/isbsib/proteomics/mzviz/qc/models/EliteMonitor.scala
@@ -0,0 +1,7 @@
+package ch.isbsib.proteomics.mzviz.qc.models
+
+/**
+ * Created by qjolliet on 06/01/16.
+ *  */
+
+case class EliteMonitorInfo(monitorDate:String,monitorIndex:String,Multplier1:String,Multplier2:String,Comment:String)
